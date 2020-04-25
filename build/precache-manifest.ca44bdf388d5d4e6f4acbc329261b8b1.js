b78087a7d4e64d790172b98b62bb230e745cdae0
phillipstemmlar <phillipstemmlar@gmail.com>
2020-04-26 01:05:57 +0200

    serve

---
+++ build/precache-manifest.ca44bdf388d5d4e6f4acbc329261b8b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1b3b0027a7eca2af7791d615722cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb67a41542231747faa",
+    "url": "/static/js/2.ec7b8294.chunk.js"
+  },
+  {
+    "revision": "0478e110cd9d8e6a5c2aac24bd2adc16",
+    "url": "/static/js/2.ec7b8294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fd50e94e1be0f2130a",
+    "url": "/static/js/main.245669d7.chunk.js"
+  },
+  {
+    "revision": "f5239f07adbbbff0d48d",
+    "url": "/static/js/runtime-main.1f3395d9.js"
+  }
+]);
